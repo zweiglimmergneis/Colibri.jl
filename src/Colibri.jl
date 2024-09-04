7cf937e84d3ee46f26024103125d869286fe9784
zweiglimmergneis <georg.hertkorn@t-online.de>
2024-09-04 21:11:54 +0200

    Add external packages, create first route

--- src/Colibri.jl
+++ src/Colibri.jl
@@ -1,5 +1,17 @@
 module Colibri
 
-# Write your package code here.
+using Oxygen
+using HTTP
+
+function welcome()
+    println("This is Colibri, a minimal web framework")
+end
+
+function webinit(req::HTTP.Request)
+    msg = "This is Colibri in your Browser"
+    return msg
+end
+
+@get "/start" webinit
 
 end
